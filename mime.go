1ea74cc24c1f70b951f5d202c45bfcd2b277f8d6
Nick Galbreath <nickg@signalsciences.com>
2016-01-26 17:09:51 -0800

    extract by mime type

---
+++ mime.go
@@ -0,0 +1,35 @@
+package plaintext
+
+import (
+	"strings"
+)
+
+// returns the mimetype of the full filename if none
+func getSuffix(filename string) string {
+	idx := strings.LastIndex(filename, ".")
+	if idx == -1 || idx+1 == len(filename) {
+		return filename
+	}
+	return filename[idx+1:]
+}
+
+// ExtractorByFilename returns an plaintext extractor based on
+// filename heuristic
+func ExtractorByFilename(filename string) (Extractor, error) {
+	var e Extractor
+	var err error
+	switch getSuffix(filename) {
+	case "md":
+		e, err = NewMarkdownText()
+	case "html":
+		e, err = NewHTMLText()
+	case "go", "h", "c", "java":
+		e, err = NewGolangText()
+	default:
+		e, err = NewIdentity()
+	}
+	if err != nil {
+		return nil, err
+	}
+	return e, nil
+}
