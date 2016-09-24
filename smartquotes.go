66094287477c61cc5c51e92887c7be26d3d2c655
Nick Galbreath <nickg@signalsciences.com>
2016-09-24 09:42:12 +0000

    add smartquotes converter

---
+++ smartquotes.go
@@ -0,0 +1,26 @@
+package plaintext
+
+import (
+	"strings"
+)
+
+var toStraight *strings.Replacer
+var toCurly *strings.Replacer
+
+func init() {
+	convertions := []string{
+		"\u2013", "-", // en dash
+		"\u2014", "-", // em dash
+		"\u2018", "'", // left single quotation mark
+		"\u2019", "'", // right single quotation mark
+		"\u201C", "\"", // left double quotation mark
+		"\u201D", "\"", // right double quotation mark
+	}
+	toStraight = strings.NewReplacer(conversions)
+}
+
+// StraightQuotes converts maybe fancy typographical characters into their
+// ASCII equivalent
+func StraightQuotes(s string) string {
+	return toStraight(s)
+}
